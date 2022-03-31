c090b2569042d53162ca1d4799d07c77f2a5c51d
Mack Kaputo <mackkaputo@gmail.com>
2022-03-31 21:15:41 +0200

    installed dependencies

---
+++ src/index.js
@@ -0,0 +1,31 @@
+/**
+=========================================================
+* Otis Admin PRO - v2.0.0
+=========================================================
+
+* Product Page: https://material-ui.com/store/items/otis-admin-pro-material-dashboard-react/
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
+
+// Soft UI Context Provider
+import { MaterialUIControllerProvider } from "context";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <MaterialUIControllerProvider>
+      <App />
+    </MaterialUIControllerProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
